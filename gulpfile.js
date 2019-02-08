50d9373d6eba35555dca0b6ba682b1be0e15a939
Jonathan Sexton <joncsexton@gmail.com>
2019-02-08 05:28:43 -0600

    working on getting the sass to compile/gulp watch correctly

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,19 @@ const gulp = require("gulp");
 const cleanCSS = require("gulp-clean-css");
 const sass = require("gulp-sass");
 
+sass.compiler = require('node-sass');
+
+// function compileSass() {
+//   return gulp
+//     .src("./assets/css/styles.scss")
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('./assets/css'))
+// }
 function compileSass() {
-  return gulp
-    .src("./assets/css/styles.css")
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'))
+  return gulp 
+  .src('./assets/lib/styles.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./assets/css'))
 }
 
 function minifyCSS() {
@@ -16,10 +24,12 @@ function minifyCSS() {
     .pipe(gulp.dest("./assets/minified"));
 }
 
-gulp.task("compileSass", "minify-css", minifyCSS);
+gulp.task("compileSass", compileSass);
+gulp.task("minify-css", minifyCSS);
 
 gulp.task("watch", () => {
   gulp.watch("./assets/minified/*.css", minifyCSS);
+  // gulp.watch("./assets/lib/styles.scss", compileSass);
 });
 
-gulp.task("default", gulp.series("compileSass", "minify-css", "watch"));
+gulp.task("default", gulp.series("compileSass","minify-css", "watch"));
