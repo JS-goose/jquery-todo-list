c47c7d7cf370d176f1a5e80a52433c00538cfcf1
Jonathan Sexton <joncsexton@.com>
2019-02-08 17:00:25 -0600

    adding todos functionality working correctly

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -1,6 +1,18 @@
 $(".todo-delete-button").on("click", function(event) {
-    event.stopPropagation();
-    $(this).parent().fadeOut(500, function(){
-        this.remove();
+  event.stopPropagation();
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      this.remove();
     });
+});
+
+$('input[type="text"]').on("keypress", function(event) {
+  const code = event.keyCode || event.which;
+  const ul = $('#todo-list');
+  const html = "<li class='todo-item'><span class='todo-delete-button'>X</span>"+$(this).val()+"</li>"
+  if (code === 13) {
+    ul.append(html);
+    $(this).val('');
+  }
 });
