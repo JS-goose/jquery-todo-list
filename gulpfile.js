ad73d5133218b3a1f755923800a35e27b3ccfb26
Jonathan Sexton <joncsexton@gmail.com>
2019-02-08 05:47:41 -0600

    compile works but watch is still wonky - needs work

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,9 @@ const sass = require("gulp-sass");
 
 sass.compiler = require('node-sass');
 
-// function compileSass() {
-//   return gulp
-//     .src("./assets/css/styles.scss")
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('./assets/css'))
-// }
 function compileSass() {
   return gulp 
-  .src('./assets/lib/styles.scss')
+  .src('./assets/css/styles.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest('./assets/css'))
 }
@@ -28,8 +22,9 @@ gulp.task("compileSass", compileSass);
 gulp.task("minify-css", minifyCSS);
 
 gulp.task("watch", () => {
-  gulp.watch("./assets/minified/*.css", minifyCSS);
-  // gulp.watch("./assets/lib/styles.scss", compileSass);
+  gulp.watch("./assets/css/styles.scss", compileSass);
+  gulp.watch("./assets/css/*.css", minifyCSS);
+  // gulp.watch("./assets/minified/*.css", minifyCSS);
 });
 
 gulp.task("default", gulp.series("compileSass","minify-css", "watch"));
