772507bc4151e2c979edfd9e6afd9de8b999dc7d
Jonathan Sexton <joncsexton@.com>
2019-02-09 10:35:47 -0600

    code format change

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -1,19 +1,23 @@
 $("ul").on("click", "li", function(event) {
-  $(this).toggleClass('deleted');
+  $(this).toggleClass("deleted");
 });
 
-$("ul").on("click", "span", function(event){
-    $(this).fadeOut(500, function() {
-        $(this).parent().remove();
-      });
-      event.stopPropagation();
-})
+$("ul").on("click", "span", function(event) {
+  $(this).fadeOut(500, function() {
+    $(this)
+      .parent()
+      .remove();
+  });
+  event.stopPropagation();
+});
 
 $('input[type="text"]').on("keypress", function(event) {
   const code = event.keyCode || event.which;
   const ul = $("#todo-list");
   const html =
-    "<li class='todo-item'><span class='todo-delete-button'><i class='fa fa-times' aria-hidden='true'></i></span>" + $(this).val() + "</li>";
+    "<li class='todo-item'><span class='todo-delete-button'><i class='fa fa-times' aria-hidden='true'></i></span>" +
+    $(this).val() +
+    "</li>";
   if (code === 13) {
     ul.append(html);
     $(this).val("");
